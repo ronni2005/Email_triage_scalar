8872c99a321bd91a9c795f8a44b4629074cc6331
Richa <richamishra2005d@gmail.com>
2026-04-08 12:41:16 +0530

    Use environment variable for server port

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ from inference import app
 
 def main():
     port = int(os.environ.get("PORT", 7860))
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     main()
